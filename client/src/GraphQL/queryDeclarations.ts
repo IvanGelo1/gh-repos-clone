48e380f7fb38bfe55d07db23d79e9a66e5072231
ivan_gelo <geloivan21@gmail.com>
2021-05-31 22:14:33 +0200

    feat: created query declarations for user, and repos

---
+++ client/src/GraphQL/queryDeclarations.ts
@@ -0,0 +1,45 @@
+
+const query_repos = {
+  query: `
+    query {
+      user (login:"rengil") {
+        repositories(first: 100) {
+          nodes {
+            name,
+            description,
+            isFork,
+            forkCount,
+            updatedAt
+            parent {
+              name
+              owner {
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+ `
+};
+
+const query_user = {
+  query: `
+    query {
+      user(login:"rengil") {
+        name,
+        login,
+        bio,
+        followers {
+          totalCount
+        },
+        following {
+          totalCount
+        },
+        location
+      }
+    }
+ `
+};
+
+export { query_repos }
