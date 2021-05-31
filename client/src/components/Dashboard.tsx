d64892e1812a3dd35186d54d5f77bd69463aba7f
ivan_gelo <geloivan21@gmail.com>
2021-05-31 23:45:45 +0200

    feat: fetching the data in the dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
+import { getRepos } from '../service/apiService';
+import { Repo } from '../interfaces/Repo';
 
 function Dashboard() {
+  const [ repos, setRepos ] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    (async (): Promise<void> => {
+      const repos = await getRepos();
+      setRepos(prev => repos);
+    })();
+  }, []);
+
   return (
     <div>
 
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
