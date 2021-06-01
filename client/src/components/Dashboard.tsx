6fbb002386b810b3f5b5f3e057d5d6bf701b638c
ivan_gelo <geloivan21@gmail.com>
2021-06-01 09:07:32 +0200

    feat: implemented search functionality for the repos

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -7,24 +7,39 @@ import RepoItem from './RepoItem';
 
 function Dashboard() {
   const [ repos, setRepos ] = useState<Repo[]>([]);
+  const [ displayedRepos, setDisplayedRepos ] = useState<Repo[]>([]);
+  const [ query, setQuery ] = useState<string>('');
 
   useEffect(() => {
     (async (): Promise<void> => {
       const repos = await getRepos();
       setRepos(prev => repos);
+      setDisplayedRepos(prev => repos);
     })();
   }, []);
+
+  console.log('query here', query);
+  useEffect(() => {
+    if (!query.length) setDisplayedRepos(prev => repos);
+    else {
+      setDisplayedRepos(prev => repos.filter(repo => {
+        const regex = new RegExp(`${query}`);
+        return repo.name.match(regex)?.length;
+      }));
+    }
+
+  }, [query]);
 
   return (
     <div className="dashboard-container">
       <div className="repositories-length">Repositories {repos.length}</div>
       <div className="dashboard-input">
-        <input className="text-input" type="text" placeholder="Find a repository..." />
+        <input value={query} onChange={e => setQuery(e.target.value)} className="text-input" type="text" placeholder="Find a repository..." />
       <button className="btn-new-repo">
         New
       </button>
       </div>
-      {repos.map((repo) => <RepoItem key={repo.updatedAt} repo={repo}/>)}
+      {displayedRepos.map((repo) => <RepoItem key={repo.updatedAt} repo={repo}/>)}
     </div>
   );
 };
