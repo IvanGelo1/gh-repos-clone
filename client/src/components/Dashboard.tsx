c240f23c2440287f9b6a7351b2e68eda0167a1df
ivan_gelo <geloivan21@gmail.com>
2021-06-01 03:21:28 +0200

    style: responsive design for screens up to 769 px

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -17,8 +17,8 @@ function Dashboard() {
 
   return (
     <div className="dashboard-container">
-      <div>Repositories {repos.length}</div>
-      <input type="text" />
+      <div className="repositories-length">Repositories {repos.length}</div>
+      <input className="text-input" type="text" placeholder="Find a repository..." />
       {repos.map((repo) => <RepoItem key={repo.updatedAt} repo={repo}/>)}
     </div>
   );
--- client/src/components/RepoItem.tsx
+++ client/src/components/RepoItem.tsx
@@ -4,20 +4,28 @@ import { Repo } from '../interfaces/Repo';
 
 import './RepoItem.css';
 
+import moment from 'moment';
+
 const RepoItem: React.FC<{repo: Repo}> = ({ repo }) => {
   return (
     <div className="repoitem-container">
-      <div>{repo.name}</div>
+      <div className="repo-name">{repo.name}</div>
       {
         repo.parent
         ?
-        <div>Forked from {repo.parent.owner.login}/{repo.parent.name}</div>
+        <div className="repo-fork">Forked from {repo.parent.owner.login}/{repo.parent.name}</div>
         :
         <div></div>
       }
-      <div>{repo.description}</div>
-      <div>{repo.forkCount}</div>
-      <div>Updated on {repo.updatedAt}</div>
+      <div className="repo-description">{repo.description}</div>
+      {
+        repo.forkCount > 0
+        ?
+        <div>forks {repo.forkCount}</div>
+        :
+        <div></div>
+      }
+      <div className="repo-updated">Updated on {moment(repo.updatedAt).format('ll')}</div>
     </div>
   );
 };
