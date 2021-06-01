ca2f9d30994cdff9e5c797dd92581e19322a99f9
ivan_gelo <geloivan21@gmail.com>
2021-06-01 13:30:29 +0200

    test: some more minor testing on UserCard with hardcoded data

--- client/src/components/UserCard.test.tsx
+++ client/src/components/UserCard.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import UserCard from './UserCard';
 
-test('Should render elements', () => {
+test('Should render elements', async() => {
   render(<UserCard />);
   expect(screen.getByRole('img', {name: 'User profile'})).toBeInTheDocument();
   expect(screen.getByText('name')).toBeInTheDocument();
@@ -11,4 +11,9 @@ test('Should render elements', () => {
   expect(screen.getByText('0 following')).toBeInTheDocument();
   expect(screen.getByText('0 followers')).toBeInTheDocument();
   expect(screen.getByText('location')).toBeInTheDocument();
+   await waitFor(() => {
+     expect(screen.getByText('Munich, Germany')).toBeInTheDocument();
+     expect(screen.getByText('68 following')).toBeInTheDocument();
+     expect(screen.getByText('15 followers')).toBeInTheDocument();
+    });
 });
